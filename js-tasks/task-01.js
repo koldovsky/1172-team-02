7d7ca21a39da0676925a8bc9b15cb0dda7a39bef
ivanOheruk <165327203+ivanOheruk@users.noreply.github.com>
2024-04-19 14:31:45 +0300

    Update task-01.js

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -3,3 +3,9 @@
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
 }
+
+// Мій варіант (Ivan Oheruk) ось такий:
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return +laLigaGoals + +copaDelReyGoals + +championsLeagueGoals;
+}
