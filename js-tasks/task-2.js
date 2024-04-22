3ee045973644524baf2ae550c53d8700c04d013a
Nazar Lobur <165337039+Nazarell@users.noreply.github.com>
2024-04-22 09:43:31 +0000

    remove other options from task-2.js

--- js-tasks/task-2.js
+++ js-tasks/task-2.js
@@ -1,24 +1,12 @@
 // Convert a string to an array https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
 function stringToArray(string) {
-    return string.split(/\s+/);
-}
-
-function stringToArray(string) {
     return string.split(' ');
 }
 
 stringToArray = (s) => s.split(' ')
 
 // DNA to RNA Conversion  https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-
-function DNAtoRNA(dna) {
-    return dna.replace(/T/g, 'U');
-}
-
-function DNAtoRNA(dna) {
-    return dna.split("T").join("U");
-}
 
 function DNAtoRNA(dna) {
     return dna.replaceAll('T', 'U');
@@ -34,23 +22,11 @@ var max = function (list) {
     return Math.max(...list)
 }
 
-var min = function (list) {
-    return Math.min.apply(null, list);
-}
-
-var max = function (list) {
-    return Math.max.apply(null, list);
-}
-
 // Smallest value of an array  https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
 function min(arr, toReturn) {
     let val = Math.min.apply(null, arr)
     return toReturn == 'value' ? val : arr.indexOf(val)
-}
-
-function min(arr, toReturn) {
-    return (toReturn == 'value') ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
 }
 
 // BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!! BONUS TASKS !!!
