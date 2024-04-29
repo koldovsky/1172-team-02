e1bab4ef536c0ebbf74f2cf72d68c1bc5f4b0971
PavloKandur <74611268+PavloKandur@users.noreply.github.com>
2024-04-29 13:19:14 +0000

    added js and css for burger menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function init() {
     import('./index.yoga-classes-carousel.js');
-    import('./index.main-page.classes.js')
+    import('./index.main-page.classes.js');
+    import('./index.header__burger-menu.js')
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
