e9eef49371a7a4b3127b5f9a62d00313cf7956de
Lushchykov <lushchykov@gmail.com>
2024-05-01 18:06:28 +0000

    Testimonials css js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function init() {
     import('./index.yoga-classes-carousel.js');
     import('./index.main-page.classes.js');
     import('./index.header__burger-menu.js')
+    import('./testimonials.js')
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
